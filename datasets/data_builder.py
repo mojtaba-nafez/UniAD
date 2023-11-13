58d4075d0be5ce09369257bf0315ffc308f24fe7
mojtaba nafez <mojtabanafez96@gmail.com>
2023-11-13 21:56:04 +0330

    update

--- datasets/data_builder.py
+++ datasets/data_builder.py
@@ -30,10 +30,12 @@ def build_dataloader(cfg_dataset, distributed=True):
     train_loader = None
     if cfg_dataset.get("train", None):
         train_loader = build(cfg_dataset, training=True, distributed=distributed)
+    print("train loader len", len(train_loader))
 
     test_loader = None
     if cfg_dataset.get("test", None):
         test_loader = build(cfg_dataset, training=False, distributed=distributed)
+    print("test loader len", len(test_loader))
 
     logger.info("build dataset done")
     return train_loader, test_loader
