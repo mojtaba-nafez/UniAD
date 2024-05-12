69433de8b277139320a5d013a62d2651a5a3f142
Arad Maleki <arad.m.t02@gmail.com>
2024-05-12 16:08:10 +0330

    debug

--- train_val.py
+++ train_val.py
@@ -157,6 +157,7 @@ def main():
     loaders = [train_loader, val_loader1, val_loader2]
     for loader in loaders:
         it = next(iter(loader))
+        print('it:', it)
         print(it.keys())
         print(len(it['filename']))
         print(len(it['image']))
